0ec886fa5e0e3d9148ae5e0ac254bdb8f8301ef6
RusVS1 <112542219+RusVS1@users.noreply.github.com>
2024-04-10 18:00:35 +0800

    corrected NegativeTextAnalyzer

--- src/test/java/edu/phystech/hw2/analyzer/NegativeTextAnalyzer.java
+++ src/test/java/edu/phystech/hw2/analyzer/NegativeTextAnalyzer.java
@@ -9,10 +9,11 @@ public class NegativeTextAnalyzer extends KeywordAnalyzer {
     public NegativeTextAnalyzer() {
         super(NEGATIVE_SMILES, Label.NEGATIVE);
     }
+    
     @Override
     public Label processText(String text) {
         for (String bannedWord : this.getKeywords()) {
-            if ((text.indexOf(bannedWord) == 0 ||text.indexOf(bannedWord) == text.length() - 2) && text.indexOf(' ' + bannedWord) != -1) {
+            if ((text.indexOf(bannedWord) == 0 || text.indexOf(bannedWord) == text.length() - bannedWord.length()) && text.indexOf(' ' + bannedWord) != -1) {
                 return Label.NEGATIVE;
             }
             if (text.indexOf(' ' + bannedWord + ' ') != -1) {
