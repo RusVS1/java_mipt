1a055f7620e7ed7b9acdfe228c5188df5b0749e8
RusVS1 <112542219+RusVS1@users.noreply.github.com>
2024-05-07 21:57:29 +0800

    add hw4

---
+++ src/main/java/edu/phystech/hw4/lock/CASTicketLock.java
@@ -0,0 +1,22 @@
+package edu.phystech.hw4.lock;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author kzlv4natoly
+ */
+
+public class CASTicketLock {
+    private final AtomicInteger nextTicket = new AtomicInteger();
+    private final AtomicInteger currentTicket = new AtomicInteger();
+
+    public void lock() {
+        int myTicket = nextTicket.getAndIncrement();
+        while (myTicket != currentTicket.get()) {
+        }
+    }
+
+    public void unlock() {
+        currentTicket.getAndIncrement();
+    }
+}
