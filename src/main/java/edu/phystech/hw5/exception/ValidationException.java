22ec7d238b954150ad8ab1ded0d0e0379eb87522
RusVS1 <112542219+RusVS1@users.noreply.github.com>
2024-05-25 00:38:29 +0800

    add hw5

---
+++ src/main/java/edu/phystech/hw5/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package edu.phystech.hw5.exception;
+
+/**
+ * @author kzlv4natoly
+ */
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
